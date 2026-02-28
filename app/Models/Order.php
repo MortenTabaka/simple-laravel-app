3a753252ea17c1f0740780d41ed0566360897752
Marcin Gołaszewski <marcin.tabaka14@gmail.com>
2026-02-28 15:20:49 +0100

    create Order

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Zamówienie w sklepie.
+ *
+ * @property string $status Status zamówienia.
+ * @property string $total_price Cena zamówienia PLN.
+ * @property User $user Użytkownik, który złożył zamówienie.
+ */
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+
+    protected $fillable = ['status', 'total_price', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'status' => fake()->randomElement([OrderStatus::CREATED, OrderStatus::CONFIRMED]),
+            'total_price' => fake()->randomFloat(2, 100, 2000),
+            'user_id' => User::factory(),
+        ];
+    }
+}
