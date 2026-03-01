98137a6938fb4b255fbef5141438e03cabe29852
Marcin Gołaszewski <marcin.tabaka14@gmail.com>
2026-03-01 10:29:24 +0100

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::apiResource('products', ProductController::class);
+Route::apiResource('orders', OrderController::class);
