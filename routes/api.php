38d843f9f25568b5e983bae183916812ec8e1bec
Marcin Gołaszewski <marcin.tabaka14@gmail.com>
2026-03-01 14:07:26 +0100

    reorder imports

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\OrderController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
 Route::apiResource('orders', OrderController::class);
